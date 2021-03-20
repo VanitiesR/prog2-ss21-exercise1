cc50a01edba8c0fb1cdc858e6e6eb31c96ef196e
Rafael Vanicek <vanities@pm.me>
2021-03-20 19:26:17 +0100

    Created gradeGenerator Method for further testing

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,23 +3,49 @@ import java.util.*;
 public class Result {
 
 
+
     public static List<Integer> gradingStudents(List<Integer> grades) {
-        //TODO
-        System.out.println("Studentenanzahl: " + grades.size());
+
+        System.out.println("Studentenanzahl (max. 30): " + grades.size());
         System.out.print("Before rounding\n");
-        for(int i = 0; i <= grades.size()-1; i++){
+
+
+        for (int i = 0; i <= grades.size() - 1; i++) {
             System.out.print(grades.get(i) + " ");
         }
+
         System.out.print("\nAfter rounding\n");
-        for(int i = 0; i <= grades.size()-1; i++){
-            if(grades.get(i) >= 38) {
+        for (int i = 0; i <= grades.size() - 1; i++) {
+            if (grades.get(i) >= 38) {
                 if (grades.get(i) % 5 >= 3) {
                     grades.set(i, (grades.get(i) + 5) - (grades.get(i) % 5));
                 }
             }
             System.out.print(grades.get(i) + " ");
         }
+
+        if(grades.size() > 30){
+            System.out.println("\nEs wurde/n "+ (grades.size()-30) +" Arbeit/en zu viel abgegeben. Hier ist was faul!\n");
+        }else if (grades.size() < 30){
+            System.out.println("\nEs wurde/n "+ (-grades.size()+30) +" Arbeit/en zu wenig abgegeben. Fehlt jemand?\n");
+
+        }
+
         System.out.print("\n\n");
         return grades;
     }
+
+
+    //Random grade generation => n grades, each between 0 and 100
+    public static List<Integer> gradeGenerator(int size) {
+        List<Integer> random = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 1; i <= size; i++) {
+            int temp = rand.nextInt(100);
+            random.add(temp);
+        }
+        return random;
+    }
+
+
 }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -14,18 +14,26 @@ class ResultTest {
 
     @Test
     @DisplayName("Check if the grades have been rounded properly")
-    void testgradingStudents() {
+    void testgradingStudents(){
         List<Integer> grades = new ArrayList<>(Arrays.asList(14,44,43,42,41));
         List<Integer> expected = new ArrayList<>(Arrays.asList(14,45,45,42,41));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "ERROR!");
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "1 ore more Integers have been wrongly/or not rounded!");
     }
 
     @Test
     @DisplayName("Check if grades below 38 have not been rounded")
     void testgradingStudents1(){
-        List<Integer> grades = new ArrayList<>(Arrays.asList(29,37,38,65,89));
-        List<Integer> expected = new ArrayList<>(Arrays.asList(29,37,40,65,90));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "ERROR!");
+        List<Integer> grades = new ArrayList<>(Arrays.asList(1,13,29,37,38,53,62,89));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1,13,29,37,40,55,62,90));
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "An Integer <= 37 has been rounded!");
     }
 
+    @Test
+    @DisplayName("InputHere")
+    void testgradingStudents3(){
+        ArrayList<Float> grades = new ArrayList<>();
+        //TODO
+        // 1.5,13.7,29.00,37.54,38.99,53.10,62.9,89.2
+
+    }
 }
