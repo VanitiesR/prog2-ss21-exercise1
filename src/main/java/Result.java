cbc71931df172887455d1a9108caf30883b3a2a6
Rafael Vanicek <vanities@pm.me>
2021-03-21 18:01:57 +0100

    Created third TestMethod, extended gradeGenerator Method

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -2,33 +2,51 @@ import java.util.*;
 
 public class Result {
 
+    public static List<Integer> getGrades() {
+        return grades;
+    }
+    public static void setGrades(List<Integer> grades) {
+        Result.grades = grades;
+    }
+    public static List<Integer> grades = new ArrayList<>(60);
 
+    public static List<Integer> gradingStudents(List<Integer> grades) throws Exception {
 
-    public static List<Integer> gradingStudents(List<Integer> grades) {
+        if (grades.size() > 60 || grades.size() == 0) {
+            throw new Exception();
+        }
+        for (int i = 0; i <= grades.size() - 1; i++) {
+            if (grades.get(i) > 100) {
+                System.out.println("Jemand hat zu viele Punkte!");
+            }
+        }
 
-        System.out.println("Studentenanzahl (max. 30): " + grades.size());
+        //print the amount of grades/students
+        System.out.println("Studentenanzahl (max. 60): " + grades.size());
         System.out.print("Before rounding\n");
 
-
+        //print the grades before rounding
         for (int i = 0; i <= grades.size() - 1; i++) {
             System.out.print(grades.get(i) + " ");
         }
 
+        //where the rounding/sorting takes place
         System.out.print("\nAfter rounding\n");
         for (int i = 0; i <= grades.size() - 1; i++) {
-            if (grades.get(i) >= 38) {
+            if (grades.get(i) >= 38) { //only round grades greater than or equal to 38
                 if (grades.get(i) % 5 >= 3) {
                     grades.set(i, (grades.get(i) + 5) - (grades.get(i) % 5));
                 }
+
             }
             System.out.print(grades.get(i) + " ");
         }
 
-        if(grades.size() > 30){
-            System.out.println("\nEs wurde/n "+ (grades.size()-30) +" Arbeit/en zu viel abgegeben. Hier ist was faul!\n");
-        }else if (grades.size() < 30){
-            System.out.println("\nEs wurde/n "+ (-grades.size()+30) +" Arbeit/en zu wenig abgegeben. Fehlt jemand?\n");
-
+        //check and print if more or less than 30 grades have been calculated
+        if(grades.size() > 60){
+            System.out.println("\nEs wurde/n "+ (grades.size()-60) +" Arbeit/en zu viel abgegeben. Hier ist was faul!\n");
+        }else if (grades.size() < 60){
+            System.out.println("\nEs wurde/n "+ (-grades.size()+60) +" Arbeit/en zu wenig abgegeben. Fehlt jemand?\n");
         }
 
         System.out.print("\n\n");
@@ -36,8 +54,20 @@ public class Result {
     }
 
 
-    //Random grade generation => n grades, each between 0 and 100
-    public static List<Integer> gradeGenerator(int size) {
+
+    //Random grade generation for Testing | 60 grades, each between 0 and 100
+    public static List<Integer> gradeGeneratorSixty() {
+        List<Integer> random = new ArrayList<>();
+        Random rand = new Random();
+        int size  = 60;
+        for (int i = 1; i <= size; i++) {
+            int temp = rand.nextInt(100);
+            random.add(temp);
+        }
+        return random;
+    }
+    //Random grade generation for Testing | n grades, each between 0 and 100
+    public static List<Integer> gradeGeneratorSized(int size) {
         List<Integer> random = new ArrayList<>();
         Random rand = new Random();
         for (int i = 1; i <= size; i++) {
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -14,26 +14,36 @@ class ResultTest {
 
     @Test
     @DisplayName("Check if the grades have been rounded properly")
-    void testgradingStudents(){
+    void testgradingStudents() throws Exception {
         List<Integer> grades = new ArrayList<>(Arrays.asList(14,44,43,42,41));
         List<Integer> expected = new ArrayList<>(Arrays.asList(14,45,45,42,41));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "1 ore more Integers have been wrongly/or not rounded!");
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "1 or more Integers have been wrongly/or not rounded!");
     }
+
 
     @Test
     @DisplayName("Check if grades below 38 have not been rounded")
-    void testgradingStudents1(){
+    void testgradingStudents1() throws Exception {
         List<Integer> grades = new ArrayList<>(Arrays.asList(1,13,29,37,38,53,62,89));
         List<Integer> expected = new ArrayList<>(Arrays.asList(1,13,29,37,40,55,62,90));
         assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(grades).toArray()), "An Integer <= 37 has been rounded!");
     }
 
     @Test
-    @DisplayName("InputHere")
+    @DisplayName("Check if 60+ grades have been provided")
     void testgradingStudents3(){
-        ArrayList<Float> grades = new ArrayList<>();
-        //TODO
-        // 1.5,13.7,29.00,37.54,38.99,53.10,62.9,89.2
+        try{
+            Result.setGrades(Result.gradeGeneratorSized(65)); //passed 65 grades for exception
+            Result.gradingStudents(Result.getGrades());
+        }catch(Exception e){
+            System.out.println("More than 60 or Zero grades have been provided!\n");
+        }
+        try{
+            Result.setGrades(Result.gradeGeneratorSixty()); // passed 60 grades to see difference
+            Result.gradingStudents(Result.getGrades());
+        }catch(Exception e){
+            System.out.println("More than 60 or Zero grades have been provided!\n");
+        }
 
     }
 }
